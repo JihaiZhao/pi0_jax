debug: fix the bug of state normalization in training and change the normalization method to min-max

src/openpi/policies/xarm_dual_policy.py
@@ -17,9 +17,9 @@ from openpi.models import model as _model
 def make_xarm_example() -> dict:
     """Creates a random input example for the Xarm policy."""
     return {
-        "observation_images_head": np.random.randint(256, size=(3, 480, 640), dtype=np.uint8),
-        "observation_images_left_wrist": np.random.randint(256, size=(3, 480, 640), dtype=np.uint8),
-        "observation_images_right_wrist": np.random.randint(256, size=(3, 480, 640), dtype=np.uint8),
+        "observation_images_head": np.random.randint(256, size=(3, 224, 224), dtype=np.uint8),
+        "observation_images_left_wrist": np.random.randint(256, size=(3, 224, 224), dtype=np.uint8),
+        "observation_images_right_wrist": np.random.randint(256, size=(3, 224, 224), dtype=np.uint8),
 
         "observation_states_ee_pose_left": np.random.rand(9),
         "observation_states_ee_pose_right": np.random.rand(9),
@@ -49,7 +49,7 @@ class XarmInputs(transforms.DataTransformFn):
     """Transform raw dataset inputs into the format expected by the model."""
     action_dim: int
     model_type: _model.ModelType = _model.ModelType.PI0
-    pose: bool = False  # Add pose parameter
+    pose: bool = True
     _has_printed: bool = dataclasses.field(default=False, init=False)
 
     def __call__(self, data: dict) -> dict:
@@ -59,41 +59,19 @@ class XarmInputs(transforms.DataTransformFn):
             print(f"Key: {key}, Value type: {type(value)}, Value shape: {value.shape if hasattr(value, 'shape') else 'N/A'}")
         # Print state type message only once
         if not object.__getattribute__(self, '_has_printed'):
-            print("Using pose as state" if self.pose else "Using joint angle as state")
+            print("Using pose to train the model" if self.pose else "Using joint angle to train the model")
             object.__setattr__(self, '_has_printed', True)
         
-        if self.pose:
-            # Get state from raw inputs using pose data
-            state_components = [
-                data["observation_states_ee_pose_left"],
-                data["observation_states_gripper_position_left"],
-                data["observation_states_ee_pose_right"],
-                data["observation_states_gripper_position_right"]
-            ]
-            state = np.concatenate(state_components)
-            state = transforms.pad_to_dim(state, self.action_dim)
-        else:
-            # Get state from raw inputs using joint angles
-            state_components = [
-                data["observation_states_joint_angle_left"],
-                data["observation_states_gripper_position_left"],
-                data["observation_states_joint_angle_right"],
-                data["observation_states_gripper_position_right"]
-            ]
-            state = np.concatenate(state_components)
-            # print(f"Original state shape: {state.shape}")  # Should be (..., 16)
-            state = transforms.pad_to_dim(state, self.action_dim)
-            # print(f"Padded state shape: {state.shape}")    # Should be (..., 32)
+        # Get state from raw inputs using pose data
+        state_components = [
+            data["observation_states_ee_pose_left"],
+            data["observation_states_gripper_position_left"],
+            data["observation_states_ee_pose_right"],
+            data["observation_states_gripper_position_right"]
+        ]
+        state = np.concatenate(state_components)
+        state = transforms.pad_to_dim(state, self.action_dim)
 
-        # state_components = [
-        #     data["observation_states_ee_pose_left"],
-        #     data["observation_states_gripper_position_left"],
-        #     data["observation_states_ee_pose_right"],
-        #     data["observation_states_gripper_position_right"]
-        # ]
-        # state = np.concatenate(state_components)
-        # state = transforms.pad_to_dim(state, self.action_dim)
-        # Process images
         base_image = _parse_image(data["observation_images_head"])
         left_wrist_image = _parse_image(data["observation_images_left_wrist"])
         right_wrist_image = _parse_image(data["observation_images_right_wrist"])
@@ -115,24 +93,26 @@ class XarmInputs(transforms.DataTransformFn):
         if "prompt" in data:
             inputs["prompt"] = data["prompt"]
 
-        # if "action_left" in data and "action_right" in data:
-        #     action_left = np.asarray(data["action_left"])
-        #     action_right = np.asarray(data["action_right"])
-        #     actions = np.concatenate([action_left, action_right], axis=-1)
-        #     inputs["actions"] = actions
+        # use pose or joint angle to train the model
+        if self.pose:
+            if "action_left" in data:
+                action_left = np.asarray(data["action_left"])
+                inputs["action_left"] = action_left
 
-        # if "actions" in data:
-        #     print(f"actions: {data['actions'].shape}")
-        #     actions = np.asarray(data["actions"])
-        #     data["actions"] = transforms.pad_to_dim(actions, self.action_dim)
+            if "action_right" in data:
+                action_right = np.asarray(data["action_right"])
+                inputs["action_right"] = action_right
+        else:
+            if "observation_states_joint_angle_left" in data:
+                action_left = np.asarray(data["observation_states_joint_angle_left"])
+                action_left = np.concatenate([action_left, data["observation_states_gripper_position_left"]], axis=-1)
+                print("action_left", action_left)
+                inputs["action_left"] = action_left
 
-        if "action_left" in data:
-            action_left = np.asarray(data["action_left"])
-            inputs["action_left"] = action_left
-
-        if "action_right" in data:
-            action_right = np.asarray(data["action_right"])
-            inputs["action_right"] = action_right
+            if "observation_states_joint_angle_right" in data:
+                action_right = np.asarray(data["observation_states_joint_angle_right"])
+                action_right = np.concatenate([action_right, data["observation_states_gripper_position_right"]], axis=-1)
+                inputs["action_right"] = action_right
 
 
         return inputs

src/openpi/training/data_loader.py
@@ -127,6 +127,9 @@ def transform_dataset(dataset: Dataset, data_config: _config.DataConfig, *, skip
                 "Make sure to run `scripts/compute_norm_stats.py --config-name=<your-config>`."
             )
         norm_stats = data_config.norm_stats
+    
+    for key in norm_stats:
+        print(f"Normalization stats for {key}: {norm_stats[key]}")
 
     transformed = TransformedDataset(
         dataset,
@@ -170,6 +173,13 @@ def create_data_loader(
     
     try:
         dataset = transform_dataset(dataset, data_config, skip_norm_stats=skip_norm_stats)
+        print("dataset keys: ", dataset[0].keys())
+        print("dataset[0]['state'] max", dataset[0]['state'].max())
+        print("dataset[0]['state'] min", dataset[0]['state'].min())
+        print("dataset[0]['image'] ", dataset[0]['image'].keys())
+        print("dataset[0]['image']['base_0_rgb'] ", dataset[0]['image']['base_0_rgb'].max())
+        print("dataset[0]['image']['left_wrist_0_rgb'] ", dataset[0]['image']['left_wrist_0_rgb'].max())
+        print("dataset[0]['image']['right_wrist_0_rgb'] ", dataset[0]['image']['right_wrist_0_rgb'].max())
         print("\nTransform successful")
     except Exception as e:
         print("\nError during transform:", str(e))
@@ -198,19 +208,34 @@ def create_data_loader(
             print("Starting data loader iteration...")
             for batch in self._data_loader:
                 print("Processing batch...")
+                # Create a copy of the batch for modification
+                batch_with_actions = batch.copy()
+                
+                # Get model action_dim from the config
+                model_action_dim = 32  # Default value for Pi0Config - correct for pi0_xarm_dual
+                
                 # Only combine actions if both left and right actions exist
                 if "action_left" in batch and "action_right" in batch:
                     actions = np.concatenate([batch["action_left"], batch["action_right"]], axis=-1)
-                    actions = _transforms.pad_to_dim(actions, 32)
-                    batch_with_actions = {**batch, "actions": actions}      
+                    actions = _transforms.pad_to_dim(actions, model_action_dim)
+                    batch_with_actions["actions"] = actions
                 else:
-                    # Use existing actions if they exist, otherwise use an empty batch
-                    batch_with_actions = {**batch, "actions": batch["action"]}
+                    # Use existing actions if they exist
+                    if "action" in batch:
+                        batch_with_actions["actions"] = batch["action"]
+                
+                # # Combine state components if they exist
+                # if "observation_states_ee_pose_left" in batch and "observation_states_ee_pose_right" in batch:
+                #     state = np.concatenate([batch["observation_states_ee_pose_left"], 
+                #                             batch["observation_states_gripper_position_left"],
+                #                             batch["observation_states_ee_pose_right"],
+                #                             batch["observation_states_gripper_position_right"]], axis=-1)
+                #     state = _transforms.pad_to_dim(state, model_action_dim)
+                #     batch_with_actions["state"] = state
+                
                 print("Batch processed, yielding...")
 
-                # actions = batch["actions"]
-                # actions = _transforms.pad_to_dim(actions, 32)
-                # batch_with_actions = {**batch, "actions": actions}
+                # Image will be normalized to [-1, 1] in the model pipeline
                 yield _model.Observation.from_dict(batch_with_actions), batch_with_actions["actions"]
 
 